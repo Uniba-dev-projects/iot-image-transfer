06477f5595a49cabb22dee1ef8d9af5e8ca3d679
lcorallo <l.corallo2@gmail.com>
2022-11-04 21:27:09 +0100

    Defining ENV file

---
+++ env.php
@@ -0,0 +1,10 @@
+<?php
+define("PROJECT_ROOT_PATH", __DIR__ . "/./");
+define("PROJECT_ROOT_NAME", "/iot-image-transfer");
+define("TEMP_FILE_NAME", "temp-file-size");
+define("TEMP_FILE_FORMAT", ".ini");
+define("TEMP_FILE", TEMP_FILE_NAME.TEMP_FILE_FORMAT);
+define("OUTPUT_FILE", "out");
+define("RGB_FORMAT", ".JPG");
+define("NIR_FORMAT", ".BIN");
+?>
